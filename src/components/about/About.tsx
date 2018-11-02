d4b28c4ac5bf5cee1037ade590f6931d6e3e089c
Matija Maric <sirSlani@gmail.com>
2018-11-02 20:53:14 +0100

    aaa

---
+++ src/components/about/About.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+class About extends React.Component {
+  public render(): React.ReactNode {
+    return <div className="about-container" />;
+  }
+}
+
+export default About;
