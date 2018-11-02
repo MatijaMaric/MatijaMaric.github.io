07c36fbc5cc4b52eb1d5a66e7b94330b7d485409
Matija Maric <sirSlani@gmail.com>
2018-11-02 20:55:07 +0100

    reset

--- src/components/about/About.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-class About extends React.Component {
-  public render(): React.ReactNode {
-    return <div className="about-container" />;
-  }
-}
-
-export default About;
--- src/components/pageNotFound/PageNotFound.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-class PageNotFound extends React.Component {
-  public render(): React.ReactNode {
-    return <div>Page not found LOL</div>;
-  }
-}
-
-export default PageNotFound;
