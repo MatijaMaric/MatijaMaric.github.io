db518e00a0d1038dc3f1e9625a485572358138c2
Matija Maric <sirSlani@gmail.com>
2018-11-02 21:22:54 +0100

    res

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss";
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        lol
+      </div>
+    );
+  }
+}
+
+export default App;
